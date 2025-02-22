720764fef99b3835c7a53d5009ab2e00ac05c96c
Ostap Sulyk <ostapsulyk@gmail.com>
2025-02-21 19:25:20 -0500

    implemented handlers

---
+++ pkg/handlers/handlers.go
@@ -0,0 +1,16 @@
+package handlers
+
+import (
+	"net/http"
+	"udemygo/pkg/render"
+)
+
+// Home handles the home page request.
+func Home(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "home.page.tmpl")
+}
+
+// About handles the about page request.
+func About(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "about.page.tmpl")
+}
